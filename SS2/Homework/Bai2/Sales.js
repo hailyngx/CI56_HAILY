a854a54d3106837c02d26dcd8b897e63d22b0bf1
Ly H Nguyen <hly.ng@Apples-MacBook-Pro-3.local>
2021-01-25 13:24:18 +0700

    bai2

--- SS2/Homework/Bai2/Sales.js
+++ SS2/Homework/Bai2/Sales.js
@@ -27,12 +27,28 @@ delete(matHang) {
 doanhThu() {
     let tongTien = 0;
     for (let i = 0; i < this.banHang.length; i++) {
-        tongTien = tongTien + (this.banHang[i].giaThanh * (1 - this.banHang[i].giamGia);
-    }
-    {
-        return tongTien; 
-    }        
-    } 
+        tongTien = tongTien + (this.banHang[i].giaThanh * (1 - this.banHang[i].giamGia);}
+        {
+            return tongTien; 
+        }       
+}
 }
 
+let food1 = new Food("Food", "Snack", 65000, "Pringles", date, "Chua");
+let food2 = new Food("Food", "Keo", 15000, "Hai Ha", date, "Ngot");
+let household1 = new Household("Household", "Chao", 200000, "Bosch", date, "15 nam");
+let household2 = new Household("Household", "Choi", 100000, "Hang Viet Nam", date, "10 nam");
+let cloth1 = new Clothing("Clothing", "Shirt", 200000, "Zara", date, "Viet Nam", "Polyester");
+let cloth2 = new Clothing("Clothing", "Sweater", 350000, "Uniqlo", date, "Nhat Ban", "Len");
 
+let sales = new Sales();
+Sales.add(food1);
+Sales.add(food2);
+Sales.add(household1);
+Sales.add(household2);
+Sales.add(cloth1);
+Sales.add(cloth2);
+Sales.delete(cloth2)
+
+Sales.doanhThu()
+console.log(Sales);
